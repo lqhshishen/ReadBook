7f47e7d61b9072127a6741196e0f94352325bc7a
liqihao <543376397@qq.com>
2017-11-14 16:23:55 +0800

    打开本地小说

--- app/src/main/java/com/liqihao/readbook/MainActivity.java
+++ app/src/main/java/com/liqihao/readbook/MainActivity.java
@@ -1,10 +1,18 @@
 package com.liqihao.readbook;
 
 import android.app.Activity;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
 import android.widget.LinearLayout;
 import android.widget.Toolbar;
+
+import java.io.InputStream;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,16 +25,84 @@ public class MainActivity extends AppCompatActivity {
 //        LinearLayout main;
 //    BindView(R.id.toolbar)
 //        android.support.v7.widget.Toolbar toolbar;
-    LinearLayout Ln;
+    @BindView(R.id.pv)
+        PageView pv;
 
     android.support.v7.widget.Toolbar toolbar;
 
+    private com.liqihao.readbook.PageFactory mPageFactory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDayTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mContext = MainActivity.this;
         ButterKnife.bind(mContext);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        Book book = new Book("chenxizhijian","/storage/emulated/0/Download/晨曦之剑.txt");
+        book.setEncoding("UTF-8");
+        pv.setSystemUiVisibility(View.INVISIBLE);
+        mPageFactory = com.liqihao.readbook.PageFactory.getInstance(pv,book);
+        mPageFactory.nextPage();
+        Log.i("a112233",book.getPath());
+
+        pv.setOnClickCallback(new PageView.OnClickCallback() {
+            @Override
+            public void onLeftClick() {
+                mPageFactory.prePage();
+            }
+
+            @Override
+            public void onMiddleClick() {
+
+            }
+
+            @Override
+            public void onRightClick() {
+                mPageFactory.nextPage();
+            }
+        });
+        pv.setOnScrollListener(new PageView.OnScrollListener() {
+            @Override
+            public void onLeftScroll() {
+                mPageFactory.nextPage();
+            }
+
+            @Override
+            public void onRightScroll() {
+                mPageFactory.prePage();
+            }
+        });
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPageFactory.saveBookmark();
+    }
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        if(isActionB)
+//    }
+
+    private BottomSheetBehavior<CardView> bottomSheetBehavior;
+    private int originPosition = -1;
+
+    private boolean hadOtherWidgetShown() {
+        if(isAnimating) {
+            return true;
+        }
+        if(bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_EXPANDED) {
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            originPosition = -1;
+            return true;
+        }
+        return false;
+    }
+    private boolean isAnimating;
+
+
 }
--- app/src/main/java/com/liqihao/readbook/PageFactory.java
+++ app/src/main/java/com/liqihao/readbook/PageFactory.java
@@ -6,15 +6,20 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.io.UnsupportedEncodingException;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by liqihao on 2017/11/13.
@@ -87,12 +92,13 @@ public class PageFactory {
         begin = spHelper.getBookmarkStart(book.getName());
         end = spHelper.getBookmarkEnd(book.getName());
         File file = new File(book.getPath());
+
         fileLength = (int) file.length();
         try {
             randomFile = new RandomAccessFile(file,"r");
             mappedFile = randomFile.getChannel().map(FileChannel.MapMode.READ_ONLY,0,(long)fileLength);
         } catch (Exception e) {
-            e.printStackTrace();
+            Log.e("nmb",Log.getStackTraceString(e));
             Toast.makeText(mContext, "打开失败!", Toast.LENGTH_SHORT).show();
         }
     }
@@ -178,6 +184,162 @@ public class PageFactory {
                     break;
                 }
             }
+            if(strParagraph.length() > 0) {
+                try {
+                    end -= (strParagraph).getBytes(encoding).length;
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    //上翻页
+    private void pageUp() {
+        String strParagraph="";
+        List<String> tempList = new ArrayList<>();
+        while (tempList.size() < lineNumber && begin > 0) {
+            byte[] byteTemp = readParagraphBack(begin);
+            begin -= byteTemp.length;
+            try {
+                strParagraph = new String(byteTemp,encoding);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            strParagraph = strParagraph.replaceAll("\r\n","  ");
+            strParagraph = strParagraph.replaceAll("\n","  ");
+            while(strParagraph.length() > 0){
+                int size = mPaint.breakText(strParagraph,true,pageWidth,null);
+                tempList.add(strParagraph.substring(0, size));
+                strParagraph = strParagraph.substring(size);
+                if(tempList.size() >= lineNumber){
+                    break;
+                }
+            }
+            if(strParagraph.length() > 0){
+                try{
+                    begin+= strParagraph.getBytes(encoding).length;
+                }catch (UnsupportedEncodingException u){
+                    u.printStackTrace();
+                }
+            }
+        }
+    }
+    public void printPage() {
+        if (content.size() > 0) {
+            int y = margin;
+            mCanvas.drawColor(mContext.getResources().getColor(R.color.dayModeTextColor));
+            for(String line : content) {
+                y += fontSize+lineSpace;
+                mCanvas.drawText(line,margin,y,mPaint);
+            }
+            float percent = (float) begin / fileLength *100;
+            DecimalFormat format = new DecimalFormat("#0.00");
+            String readingProgress = format.format(percent) + "%";
+            int length = (int) mPaint.measureText(readingProgress);
+            mCanvas.drawText(readingProgress,(screenWidth - length) / 3
+                    ,screenHeight - margin,mPaint);
+        }
+    }
+
+    private int serchPositionByKey (int beginPos,String key) {
+        int position = begin;
+        try {
+            randomFile.seek(beginPos);
+            String keyString = new String(key.getBytes(encoding), StandardCharsets.ISO_8859_1);
+            String temp;
+            long pointer;
+            for(;;) {
+                pointer = randomFile.getFilePointer();
+                temp = randomFile.readLine();
+                if(temp != null && temp.contains(keyString)) {
+                    position = (int) pointer;
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return position;
+    }
+    public void nextPage() {
+        if (end >= fileLength) {
+            return;
+        }else{
+            content.clear();
+            begin = end;
+            pageDown();
+        }
+        printPage();
+    }
+    public void prePage() {
+        if(begin <= 0) {
+            return;
+        }else{
+            content.clear();
+            pageUp();
+            end = begin;
+            pageDown();
+        }
+        printPage();
+    }
+    public void saveBookmark() {
+        SPHelper.getInstance().setBookmarkEnd(book.getName(),begin);
+        SPHelper.getInstance().setBookmarkStart(book.getName(),begin);
+    }
+    public int getFileLength() {
+        return fileLength;
+    }
+    public MappedByteBuffer getMappedFile() {
+        return mappedFile;
+    }
+
+    public void setPosition(int position) {
+
+        end = position;
+        nextPage();
+    }
+    public int getProgress() {
+        return begin * 100 / fileLength;
+    }
+    public int setProgress(int i) {
+        int origin = begin;
+        end = fileLength * i/100;
+        if (end == fileLength) {
+            end--;
+        }
+        if(end == 0) {
+            nextPage();
+        }else{
+            nextPage();
+            prePage();
+            nextPage();
+        }
+        return origin;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+    public String getEncoding() {
+        return encoding;
+    }
+    public int getCurrentEnd() {
+        return end;
+    }
+    public int getCurrentBegin() {
+        return begin;
+    }
+
+    public static void close() {
+        if(instance != null) {
+            try {
+                instance.randomFile.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            instance = null;
         }
     }
 }
